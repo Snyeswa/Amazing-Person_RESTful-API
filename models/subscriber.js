1a599ad5cdb31b460536c4e05ffe672425507067
Sylvester Nyeswa <sylvesternyeswa.student@careerdevs.com>
2020-01-29 19:01:15 -0500

    setup a Nodejs with express, connect to mongodb

---
+++ models/subscriber.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+        
+    },
+    subscriberToChannel: {
+        type: String,
+        required: true
+
+    },
+    subscriberDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+
+    }
+})
+module.exports = mongoose.model('Teacher', subscriberSchema)
